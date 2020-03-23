59517c7f98a15ba6c7c992c94f7533c927aed59b
Auto <auto@zju.edu.cn>
2020-03-23 20:12:38 +0800

    up

---
+++ QuantumultX/itranslate.js
@@ -0,0 +1,15 @@
+/*
+QX:
+^https:\/\/ssl-api\.itranslateapp\.com\/.*\/subscriptions\/.*\/ios$ url script-response-body itranslate.js
+
+MitM = ssl-api.itranslateapp.com
+
+*/
+
+var obj = JSON.parse($response.body);
+
+obj= {"licenses": [{"is_trial_period": true, "product_id": "com.itranslate.pro.yearly", "original_transaction_id": "20000627379146", "expires_date_ms": 4099246456000, "bundle_id": "com.outerspaceapps.itranslate", "transaction_id": "20000627379146"}, {"is_trial_period": true, "product_id": "com.itranslate.pro.yearly", "original_transaction_id": "20000627379146", "expires_date_ms": 4099246456000, "bundle_id": "com.outerspaceapps.itranslate", "transaction_id": "20000627379146"}]};
+
+$done({body: JSON.stringify(obj)});
+
+// Descriptions
